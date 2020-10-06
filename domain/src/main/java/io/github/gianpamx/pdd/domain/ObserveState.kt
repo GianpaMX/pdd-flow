56e34b5bcb30313da6e4754ec55ac9e0eac14172
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-05 21:26:03 -0500

    Add 'onStart' method to flow.

--- domain/src/main/java/io/github/gianpamx/pdd/domain/ObserveState.kt
+++ domain/src/main/java/io/github/gianpamx/pdd/domain/ObserveState.kt
@@ -12,10 +12,7 @@ import io.github.gianpamx.pdd.domain.entity.State.POMODORO
 import io.github.gianpamx.pdd.domain.entity.Transition
 import io.github.gianpamx.pdd.domain.entity.ZenMode
 import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 
 private const val POMODORO_LENGTH = 25 * 60
 private const val BREAK_LENGTH = 5 * 60
@@ -42,6 +39,11 @@ class ObserveState(
         .ticker()
         .combine(transitionApi.observeTransitionLog()) { now, transition ->
             transition.toState(now)
+        }
+        .onStart {
+            transitionApi.getLastTransition()?.let {
+                emit(it.toState(timeApi.now()))
+            }
         }
         .distinctUntilChanged()
         .onEach {
